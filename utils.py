8691d601b136fb1a05eede50cedeeefe4e70f66a
Daphne Liu <daphnehliu13@gmail.com>
2024-03-18 16:14:20 -0700

    milestone code

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import numpy as np
 
 def detect_face_coords(img, classifier):
     gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # TODO add parameter optimization
     coords = classifier.detectMultiScale(gray_frame, 1.3, 5)
 
     biggest = (0, 0, 0, 0)
@@ -15,20 +17,38 @@ def detect_face_coords(img, classifier):
 
 def detect_eyes_coords(img, classifier):
     gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    eyes = classifier.detectMultiScale(gray_frame, 1.3, 5) # detect eyes
+    
     width = np.size(img, 1) # get face frame width
     height = np.size(img, 0) # get face frame height
     left_eye = (0, 0, 0, 0)
     right_eye = (0, 0, 0, 0)
+    
+    # find the best detectMultiScale parameters
+    scaleFactors = [1.01, 1.1, 1.2, 1.3, 1.4, 1.5]
+    minNeighbors = [3, 4, 5, 6]
+    for sf in scaleFactors:
+        for mn in minNeighbors:
+            eyes = classifier.detectMultiScale(gray_frame, sf, mn, 0, (int(width/5), int(height/5))) # detect eyes
+            if len(eyes) >= 2:
+                if abs(eyes[0][1] - eyes [1][1]) < 50:
+                    for (x, y, w, h) in eyes:
+                        if y < height / 2: 
+                            eyecenter = x + w / 2  # get the eye center
+                            if eyecenter < width * 0.5:
+                                left_eye = (x, y, w, h)
+                            else:
+                                right_eye = (x, y, w, h)
+                    return left_eye, right_eye
+    
+    '''eyes = classifier.detectMultiScale(gray_frame, 1.3, 5) # detect eyes
     for (x, y, w, h) in eyes:
-        if y > height / 2:
-            pass
+        
         eyecenter = x + w / 2  # get the eye center
         if eyecenter < width * 0.5:
             left_eye = (x, y, w, h)
         else:
-            right_eye = (x, y, w, h)
-    return left_eye, right_eye
+            right_eye = (x, y, w, h)'''
+    return (0, 0, 0, 0), (0, 0, 0, 0)
 
 def remove_eyebrows(img):
     height, width = img.shape[: 2]
