d319d253b90be8127642631c8e24368c760d956d
arisachue <arisachue@gmail.com>
2024-03-18 17:44:33 -0700

    Change pupil detection to contour detection

--- utils.py
+++ utils.py
@@ -30,7 +30,7 @@ def detect_eyes_coords(img, classifier):
         for mn in minNeighbors:
             eyes = classifier.detectMultiScale(gray_frame, sf, mn, 0, (int(width/5), int(height/5))) # detect eyes
             if len(eyes) >= 2:
-                if abs(eyes[0][1] - eyes [1][1]) < 50:
+                if abs(eyes[0][1] - eyes[1][1]) < 50:
                     for (x, y, w, h) in eyes:
                         if y < height / 2: 
                             eyecenter = x + w / 2  # get the eye center
@@ -67,3 +67,48 @@ def find_eye_keypoints(img, detector):
 
     keypoints = detector.detect(img)
     return keypoints
+
+def find_best_threshold(gray_img):
+    start = 60
+    
+    _, eye_img = cv2.threshold(gray_img, start, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(eye_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # keeps decreasing threshold (make it stricter) until 2 contours remain (one is the whole window frame, one is the real contour)
+    while len(contours) > 2 and start >= 20:
+        start -= 2
+        _, eye_img = cv2.threshold(gray_img, start, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(eye_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
+    return start + 2
+
+# mainly shifting contours that get affected by makeup
+def shift_contour_inside_eye(eye_img, contour):
+    width = np.size(eye_img, 1) 
+    height = np.size(eye_img, 0) 
+    
+    contour_coords = contour.reshape(-1, 2)
+
+    # center contour point
+    center_x = np.mean(contour_coords[:, 0])
+    center_y = np.mean(contour_coords[:, 1])
+    
+    # getting approx boundaries of the eye
+    start_approx_eye_x = width * 0.25
+    start_approx_eye_y = height * 0.25
+    end_approx_eye_x = width * 0.75
+    end_approx_eye_y = width * 0.75
+    center_eye_x = width / 2
+    center_eye_y = height / 2
+    
+    shift_x = 0    
+    if center_x < start_approx_eye_x or center_x > end_approx_eye_x:
+        shift_x = center_eye_x - center_x
+    shift_y = 0    
+    if center_y < start_approx_eye_y or center_y > end_approx_eye_y:
+        shift_y = center_eye_y - center_y
+        
+    # shifting the contour points towards center of eye
+    contour = contour + [shift_x, shift_y]
+        
+    return contour
